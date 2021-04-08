53038629211b23f712f2b719d98cb458834e6d67
Christina <oni22del@gmail.com>
2021-04-08 12:38:16 +0300

    hookah-store-project

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -21,33 +21,33 @@ export class DashboardComponent implements OnInit {
 
   slidesData = [
     {
-      bg: 'assets/images/slide1.png',
+      bg: 'assets/images/slide-1-hookah.png',
       type: 'Табак',
       name: 'Dead Horse',
-      description: 'Смеси Dead Horse отличаются высоким уровнем крепости. Табак для кальяна Mint Mafia от бренда Dead Horse' +
+      description: 'Смеси Dead Horse отличаются высоким уровнем крепости. Табак для кальяна Mint Mafia от бренда Dead Horse ' +
         'напоминает на вкус чистую мяту. Отлично курится в соло, при желании можно разбавить.',
     },
     {
-      bg: 'assets/images/slide2.png',
+      bg: 'assets/images/slide-2-hookah.png',
       type: 'Табак',
       name: 'DarkSide',
       description: 'Манговый ласси у Darkside – это мягкий, ароматный и сладкий вкус манго с небольшой кислинкой. ' +
         'Аромат манго очень естественный, советуем попробовать!',
     },
     {
-      bg: 'assets/images/slide1.png',
+      bg: 'assets/images/slide-1-hookah.png',
       type: 'Табак',
       name: 'Dead Horse',
-      description: 'Смеси Dead Horse отличаются высоким уровнем крепости. Табак для кальяна Mint Mafia от бренда Dead Horse' +
+      description: 'Смеси Dead Horse отличаются высоким уровнем крепости. Табак для кальяна Mint Mafia от бренда Dead Horse ' +
         'напоминает на вкус чистую мяту. Отлично курится в соло, при желании можно разбавить.',
     },
   ];
 
   instagramImgs= [
-    'assets/images/inst1.png',
-    'assets/images/inst2.png',
-    'assets/images/inst3.png',
-    'assets/images/inst4.png',
+    'assets/images/insta-img-1.png',
+    'assets/images/insta-img-2.png',
+    'assets/images/insta-img-3.png',
+    'assets/images/insta-img-4.png',
   ];
 
   constructor() { }
