3ec958d7524a29620e93e092ab081beec0da3353
Christina <oni22del@gmail.com>
2021-04-10 13:10:44 +0300

    routing-issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
+import {RouterModule, Routes} from 'angular/router';
+import { InventoryPageComponent } from './src/app/shop/components/inventory-page/inventory-page.component';
+import { ItemPageComponent } from './src/app/shop/components/item-page/item-page.component';
 
+const appRoutes = [
+
+  {path: 'store', component: InventoryPageComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryPageComponent,
+    ItemPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
