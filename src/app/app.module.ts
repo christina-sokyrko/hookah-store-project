ed542cea28470997059a2684563858e20aff92c7
dar-online <andriydubinyak@gmail.com>
2021-04-18 20:45:16 +0300

    feature add accordion and fixed inventory page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
 import {InventoryPageComponent} from './shop/components/inventory-page/inventory-page.component';
 import {ItemPageComponent} from './shop/components/item-page/item-page.component';
 import {HttpClientModule} from '@angular/common/http';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InventoryPageComponent,
-    ItemPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    CoreModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        InventoryPageComponent,
+        ItemPageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        CoreModule,
+        HttpClientModule,
+        NgbModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {InventoryPageComponent} from './components/inventory-page/inventory-page.component';
 import {ItemPageComponent} from './components/item-page/item-page.component';
 
 const routes: Routes = [
-  { path: 'inventory', pathMatch: 'full', component: InventoryPageComponent },
-  { path: 'item-page', pathMatch: 'full', component: ItemPageComponent },
+    {path: 'inventory', pathMatch: 'full', component: InventoryPageComponent},
+    {path: 'item-page', pathMatch: 'full', component: ItemPageComponent},
 
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+    ]
 })
-export class ShopModule { }
+export class ShopModule {
+}
